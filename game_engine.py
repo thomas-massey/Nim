33e034fd63ee3a67638da423400d667f62fb3d20
Thomas Massey <thomassey476@outlook.com>
2022-09-11 23:28:32 +0100

    Fully functional - Restart Sequence

--- game_engine.py
+++ game_engine.py
@@ -10,54 +10,63 @@ class GameEngine:
         self.start_game()
 
     def start_game(self):
-        number_of_cards_left = 21
         # Start the window
         self.render_engine.start_window()
-        # Let the user chose wether to play first or second
-        self.go_first = self.render_engine.choose_first_or_second()
-        if self.go_first:
-            self.is_players_turn = True
-        else:
-            self.is_players_turn = False
-        # Ask if they want impossible mode
-        self.impossible_mode = self.render_engine.choose_impossible_mode()
-        # Start the game
-        self.game_active = True
-        # Start the game loop
-        while self.game_active:
-            # Draw the game
-            # So draw a card in the middle of the screen
-            self.render_engine.draw_game(number_of_cards_left, self.is_players_turn)
-            if self.is_players_turn:
-                # Check for events
-                self.move = self.render_engine.listen_for_events()
-                self.last_move = self.move
-                number_of_cards_left -= self.move
-                self.render_engine.draw_game(number_of_cards_left, self.is_players_turn)
-                sleep(1)
-                self.is_players_turn = False
+        self.restart_game_loop = True
+        while self.restart_game_loop:
+            number_of_cards_left = 21
+            # Let the user chose wether to play first or second
+            self.go_first = self.render_engine.choose_first_or_second()
+            if self.go_first:
+                self.is_players_turn = True
             else:
-                if self.impossible_mode:
-                    if number_of_cards_left % 4 == 0:
-                        self.move = random.randint(1, 3)
-                    else:
-                        self.move = 4 - self.last_move
-                    self.is_players_turn = True
+                self.is_players_turn = False
+            # Ask if they want impossible mode
+            self.impossible_mode = self.render_engine.choose_impossible_mode()
+            # Start the game
+            self.game_active = True
+            self.restart_game_loop = False
+            # Start the game loop
+            while self.game_active:
+                # Draw the game
+                # So draw a card in the middle of the screen
+                self.render_engine.draw_game(number_of_cards_left, self.is_players_turn)
+                if self.is_players_turn:
+                    # Check for events
+                    self.move = self.render_engine.listen_for_events()
+                    number_of_cards_left -= self.move
+                    self.render_engine.draw_game(number_of_cards_left, self.is_players_turn)
+                    sleep(1)
+                    self.is_players_turn = False
                 else:
-                    # Pick a random number of cards to take that is possible with the number of cards left
-                    if number_of_cards_left > 3:
-                        self.move = random.randint(1, 3)
+                    if self.impossible_mode:
+                        if number_of_cards_left % 4 == 0:
+                            # Try to make the number of cards left a multiple of 4
+                            self.move = random.randint(1, 3)
+                        else:
+                            # If the number of cards left is not a multiple of 4, then make it one
+                            self.move = number_of_cards_left % 4
+                        self.render_engine.draw_game(number_of_cards_left, self.is_players_turn)
+                        sleep(1)
+                        self.is_players_turn = True
                     else:
-                        self.move = random.randint(1, number_of_cards_left)
+                        # Pick a random number of cards to take that is possible with the number of cards left
+                        if number_of_cards_left > 3:
+                            self.move = random.randint(1, 3)
+                        else:
+                            self.move = random.randint(1, number_of_cards_left)
                     self.render_engine.display_computers_move(self.move)
                     self.is_players_turn = True
-                number_of_cards_left -= self.move
-            # Check if the game is over
-            if number_of_cards_left <= 0:
-                if self.is_players_turn:
-                    self.player_won = False
-                else:
-                    self.player_won = True
-                self.game_active = False
-                self.render_engine.draw_final_screen(self.player_won)
-                sleep(5)
+                    number_of_cards_left -= self.move
+                # Check if the game is over
+                if number_of_cards_left <= 0:
+                    if self.is_players_turn:
+                        self.player_won = False
+                    else:
+                        self.player_won = True
+                    self.game_active = False
+                    self.render_engine.draw_final_screen(self.player_won)
+                    sleep(2)
+                    # Maybe add play again option?
+                    if self.render_engine.play_again():
+                        self.restart_game_loop = True
